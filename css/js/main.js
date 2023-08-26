364fdadf1dc951a4abbd2faccf064cce583325fe
123jojfrj <138262665+123jojfrj@users.noreply.github.com>
2023-08-26 01:26:27 +0100

    Add files via upload

---
+++ css/js/main.js
@@ -0,0 +1,53 @@
+/*========================= SWIPER CATEGORIES =================*/
+var swiperCategories = new Swiper(".categories--container", {
+    spaceBetween: 24,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 6,
+          spaceBetween: 40,
+        },
+        1400: {
+          slidesPerView: 6,
+          spaceBetween: 24,
+        },
+      },
+  });
+  
+/*========== SWIPER PRODUCT ===================*/
+var swiperProducts = new Swiper(".new--container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20, 
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+    },
+}); 
+
+
+
+
+
